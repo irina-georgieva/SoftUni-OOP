d8491c197b0cc7e796d832886b5ec89feb674aad
Irina Georgieva <georgieva2490@gmail.com>
2021-10-29 23:21:43 +0200

    Added new solutions

---
+++ 03 Inheritance/Inheritance/RandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CustomRandomList
+{
+    public class RandomList: List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+        
+        public string RandomString()
+        {
+            int index = random.Next(0, Count);
+
+            var element = this[index];
+            RemoveAt(index);
+            return element;
+        }
+    }
+
+}
