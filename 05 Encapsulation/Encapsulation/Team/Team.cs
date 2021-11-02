92f812822e3006aa2a70b175fa51fb4395abb344
Irina Georgieva <georgieva2490@gmail.com>
2021-11-02 15:45:46 +0100

    Added new solutions

---
+++ 05 Encapsulation/Encapsulation/Team/Team.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyList<Person> FirstTeam
+        {
+            get => firstTeam;
+        }
+        public IReadOnlyList<Person> ReserveTeam
+        {
+            get => reserveTeam; 
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
