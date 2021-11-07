3d6ea71737b0b6aaf3ef95899c993f67735d0e89
Irina Georgieva <georgieva2490@gmail.com>
2021-11-07 11:16:32 +0100

    Added new solutions

---
+++ 07 Interfaces and Abstraction/InterfacesAndAbstraction/Cars/Tesla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get ; set ; }
+        public string Color { get ; set; }
+        public int Battery { get ; set ; }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine("Engine start");
+            sb.AppendLine("Breaaak!");
+
+            return sb.ToString();
+        }
+    }
+}
