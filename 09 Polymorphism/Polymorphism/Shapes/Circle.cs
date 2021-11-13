2763e23453ec7e45faaabd8eff00970bd4435878
Irina Georgieva <georgieva2490@gmail.com>
2021-11-13 11:53:26 +0100

    Added new solutions

---
+++ 09 Polymorphism/Polymorphism/Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * 2 * Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+    }
+}
