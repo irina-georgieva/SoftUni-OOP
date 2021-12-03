4fdb5b95885f29d19528237ebab860b10577c156
Irina Georgieva <georgieva2490@gmail.com>
2021-12-03 01:05:49 +0100

    Added new solutions

---
+++ 16 Reflection and Attributes Exercise/ReflectionAndAttributesExercise/CommandPattern/Core/Commands/BeepCommand.cs
@@ -0,0 +1,13 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern.Core.Commands
+{
+    public class BeepCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Beep {args[0]}";
+        }
+    }
+}
---
+++ 16 Reflection and Attributes Exercise/ReflectionAndAttributesExercise/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int maxValue;
+        private int minValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.maxValue = maxValue;
+            this.minValue = minValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            int inputInteger = (int)obj;
+
+            if (inputInteger < minValue || inputInteger > maxValue)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
