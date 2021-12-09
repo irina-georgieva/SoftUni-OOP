8a1cab05117a60fd8e46bb67c6ac5cc1587d92f5
Irina Georgieva <georgieva2490@gmail.com>
2021-12-09 12:31:25 +0100

    Changed the name

---
+++ 25 Exam Preparation - 15 August 2021/CarRacing/CarRacing/Core/Controller.cs
@@ -0,0 +1,106 @@
+﻿using CarRacing.Core.Contracts;
+using CarRacing.Models.Cars;
+using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Maps;
+using CarRacing.Models.Racers;
+using CarRacing.Models.Racers.Contracts;
+using CarRacing.Repositories;
+using CarRacing.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarRacing.Core
+{
+    public class Controller : IController
+    {
+        private CarRepository cars;
+        private RacerRepository racers;
+        private Map map;
+
+        public Controller()
+        {
+            cars = new CarRepository();
+            racers = new RacerRepository();
+            map = new Map();
+        }
+
+        public string AddCar(string type, string make, string model, string VIN, int horsePower)
+        {
+            ICar car;
+            if (type == "SuperCar")
+            {
+                car = new SuperCar(make, model, VIN, horsePower);
+            }
+            else if (type == "TunedCar")
+            {
+                car = new TunedCar(make, model, VIN, horsePower);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidCarType);
+            }
+
+            this.cars.Add(car);
+            return $"Successfully added car {car.Make} {car.Model} ({car.VIN}).";
+        }
+
+        public string AddRacer(string type, string username, string carVIN)
+        {
+            ICar car = this.cars.FindBy(carVIN);
+            if (car == null)
+            {
+                throw new ArgumentException(ExceptionMessages.CarCannotBeFound);
+            }            
+
+            IRacer racer;
+            if (type == "ProfessionalRacer")
+            {
+                racer = new ProfessionalRacer(username, car);
+            }
+            else if (type == "StreetRacer")
+            {
+                racer = new StreetRacer(username, car);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidRacerType);
+            }
+
+            this.racers.Add(racer);
+            return $"Successfully added racer {racer.Username}.";
+        }
+
+        public string BeginRace(string racerOneUsername, string racerTwoUsername)
+        {
+            var racerOne = this.racers.FindBy(racerOneUsername);
+            if (racerOne == null)
+            {
+                throw new ArgumentException($"Racer {racerOneUsername} cannot be found!");
+            }
+
+            var racerTwo = this.racers.FindBy(racerTwoUsername);
+            if (racerTwo == null)
+            {
+                throw new ArgumentException($"Racer {racerTwoUsername} cannot be found!");
+            }
+
+            return this.map.StartRace(racerOne, racerTwo);
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            var racers = this.racers.Models.OrderByDescending(x => x.DrivingExperience)
+                .ThenBy(x => x.Username);
+
+            foreach (var racer in racers)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
