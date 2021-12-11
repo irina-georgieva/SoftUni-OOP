81c3925a445d193916e5ba8f73ea34f9b31744f2
Irina Georgieva <georgieva2490@gmail.com>
2021-12-11 22:56:39 +0100

    Added new solutions

---
+++ 26 Exam Preparation - 22 August 2021/Astronaut/SpaceStation/Core/Controller.cs
@@ -0,0 +1,133 @@
+﻿using SpaceStation.Core.Contracts;
+using SpaceStation.Models.Astronauts;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission;
+using SpaceStation.Models.Mission.Contracts;
+using SpaceStation.Models.Planets;
+using SpaceStation.Models.Planets.Contracts;
+using SpaceStation.Repositories;
+using SpaceStation.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Core
+{
+    public class Controller : IController
+    {
+        private readonly IRepository<IAstronaut> astronautRepo;
+        private readonly IRepository<IPlanet> planetRepo;
+        private readonly IMission mission;
+        private int exploredPlanet;
+
+        public Controller()
+        {
+            this.astronautRepo = new AstronautRepository();
+            this.planetRepo = new PlanetRepository(); ;
+            this.mission = new Mission();
+        }
+
+        public Biologist IAstronaut { get; private set; }
+
+        public string AddAstronaut(string type, string astronautName)
+        {
+            IAstronaut astronaut;
+
+            if (type == "Biologist")
+            {
+                astronaut = new Biologist(astronautName);
+            }
+            else if (type == "Geodesist")
+            {
+                astronaut = new Geodesist(astronautName);
+            }
+            else if (type == "Meteorologist")
+            {
+                astronaut = new Meteorologist(astronautName);
+            }
+            else
+            {
+                throw new InvalidOperationException("Astronaut type doesn't exists!");
+            }
+
+            astronautRepo.Add(astronaut);
+            return $"Successfully added {type}: {astronautName}!";
+        }
+
+        public string AddPlanet(string planetName, params string[] items)
+        {
+            IPlanet planet = new Planet(planetName);
+
+            foreach (var item in items)
+            {
+                planet.Items.Add(item);
+            }
+
+            this.planetRepo.Add(planet);
+
+            return $"Successfully added Planet: {planetName}!";
+        }
+
+        public string ExplorePlanet(string planetName)
+        {
+            var astronauts = this.astronautRepo.Models.Where(x => x.Oxygen > 60).ToList();
+
+            if (!astronauts.Any())
+            {
+                throw new InvalidOperationException("You need at least one astronaut to explore the planet");
+            }
+
+            exploredPlanet++;
+
+            var planet = this.planetRepo.FindByName(planetName);
+            this.mission.Explore(planet, astronauts);
+
+            int deadAstronaut = 0;
+
+            foreach (var astronaut in astronauts)
+            {
+                if (!astronaut.CanBreath)
+                {
+                    deadAstronaut++;
+                }
+            }
+
+            return $"Planet: {planetName} was explored! Exploration finished with {deadAstronaut} dead astronauts!";
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{exploredPlanet} planets were explored!");
+            sb.AppendLine("Astronauts info:");
+            foreach (var astronaut in astronautRepo.Models)
+            {
+                sb.AppendLine($"Name: {astronaut.Name}");
+                sb.AppendLine($"Name: {astronaut.Oxygen}");
+
+                string itemInfo = astronaut.Bag.Items.Any() ?
+                    string.Join(", ", astronaut.Bag.Items) :
+                    "none";
+
+                sb.AppendLine($"Bag items: {itemInfo}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RetireAstronaut(string astronautName)
+        {
+            IAstronaut astronaut = this.astronautRepo.FindByName(astronautName);
+
+            if (astronaut == null)
+            {
+                throw new InvalidOperationException($"Astronaut { astronautName } doesn't exists!");
+            }
+
+            astronautRepo.Remove(astronaut);
+            return $"Astronaut {astronautName} was retired!";
+        }
+    }
+}
---
+++ 26 Exam Preparation - 22 August 2021/Astronaut/SpaceStation/Models/Mission/Mission.cs
@@ -0,0 +1,33 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission.Contracts;
+using SpaceStation.Models.Planets.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        { 
+            foreach (var astronaout in astronauts)
+            {
+                while (astronaout.CanBreath && planet.Items.Any())
+                {
+                    var item = planet.Items.FirstOrDefault();
+
+                    astronaout.Bag.Items.Add(item);
+                    planet.Items.Remove(item);
+                    astronaout.Breath();
+                }
+
+                if (!planet.Items.Any())
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
